3d0856f5041d8af0df4f52cfe210b884fb9f711d
julien ragil <julien.ragil@gmail.com>
2016-10-03 17:04:12 +0200

    BONJOUR LE MONDE

---
+++ gitmavenmodule/ProjetGITSopra/src/ProjetGITSopra/Programme.java
@@ -0,0 +1,12 @@
+package ProjetGITSopra;
+
+public class Programme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		
+	}
+
+}
